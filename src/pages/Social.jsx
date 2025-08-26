1d096e6d423d65736db5dc94f8bde01399216c81
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2025-08-26 04:46:25 -0300

    Updated Social.jsx

--- src/pages/Social.jsx
+++ src/pages/Social.jsx
@@ -39,7 +39,7 @@ const Social = () => {
             <div className="w-32 h-32 mx-auto rounded-full border-4 border-primary flex items-center justify-center mb-6">
               <div className="text-center">
                 <div className="text-3xl font-bold text-primary">1k+</div>
-                <div className="text-xs text-gray-600 font-medium">mulheres</div>
+                <div className="text-xs text-gray-600 font-medium">women</div>
               </div>
             </div>
           </div>
